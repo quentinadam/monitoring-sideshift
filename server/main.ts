eeb63e97ef613fd80231e6e5db4e7dbfe89ab37c
Quentin Adam <quentinadam@gmail.com>
2024-10-29 16:11:53 +0100

    WIP

--- server/main.ts
+++ server/main.ts
@@ -223,6 +223,7 @@ Deno.serve({ port: 80, hostname: '0.0.0.0' }, async (request) => {
     <html>
     <head>
     <title>Monitoring</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
